a25a1387d6043e7603056de35c5b1c1c74c67408
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2022-03-10 18:23:02 +0300

    feat(wasm): import smth to wasm module

--- www/index.js
+++ www/index.js
@@ -1,8 +1,14 @@
 async function init() {
-  const resWasm = await fetch('sum.wasm');
+  const objToImport = {
+    console: {
+      log: () => console.log('console.log'),
+      error: () => console.error('console.error'),
+    },
+  };
+  const resWasm = await fetch('sum_with_import.wasm');
   const buffer = await resWasm.arrayBuffer();
 
-  const wasm = await WebAssembly.instantiate(buffer);
+  const wasm = await WebAssembly.instantiate(buffer, objToImport);
   const { sum } = wasm.instance.exports;
   const res = sum(100, 1000);
   alert(res); // -> 1100
