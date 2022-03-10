4f4f16bf36c86f30a6599cf6cd17217f093695b4
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2022-03-10 18:31:05 +0300

    feat(wasm): export from wasm

--- www/index.js
+++ www/index.js
@@ -5,13 +5,18 @@ async function init() {
       error: () => console.error('console.error'),
     },
   };
-  const resWasm = await fetch('sum_with_import.wasm');
+  const resWasm = await fetch('sum_with_export.wasm');
   const buffer = await resWasm.arrayBuffer();
 
   const wasm = await WebAssembly.instantiate(buffer, objToImport);
-  const { sum } = wasm.instance.exports;
+  const { sum, mem } = wasm.instance.exports;
+
   const res = sum(100, 1000);
-  alert(res); // -> 1100
+  console.log(res); // -> 1100
+
+  const fiveBytes = new Uint8Array(mem.buffer, 0, 5);
+  const helloText = new TextDecoder().decode(fiveBytes);
+  console.log(helloText); // -> "Hello"
 }
 
 init();
