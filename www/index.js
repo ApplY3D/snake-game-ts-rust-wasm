cb751ab1ad90a3903c4d80d852050fa25b97a18a
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2022-03-10 18:38:28 +0300

    feat(js): read memory in js without export from wasm

--- www/index.js
+++ www/index.js
@@ -1,20 +1,20 @@
 async function init() {
+  const memory = new WebAssembly.Memory({ initial: 1 });
+
   const objToImport = {
+    js: {
+      mem: memory,
+    },
     console: {
       log: () => console.log('console.log'),
       error: () => console.error('console.error'),
     },
   };
-  const resWasm = await fetch('sum_with_export.wasm');
+  const resWasm = await fetch('sum_with_import_v2.wasm');
   const buffer = await resWasm.arrayBuffer();
-
   const wasm = await WebAssembly.instantiate(buffer, objToImport);
-  const { sum, mem } = wasm.instance.exports;
 
-  const res = sum(100, 1000);
-  console.log(res); // -> 1100
-
-  const fiveBytes = new Uint8Array(mem.buffer, 0, 5);
+  const fiveBytes = new Uint8Array(memory.buffer, 0, 5);
   const helloText = new TextDecoder().decode(fiveBytes);
   console.log(helloText); // -> "Hello"
 }
